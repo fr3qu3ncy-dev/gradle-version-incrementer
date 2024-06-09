de0bb7bc6edc888170d39aa8f1fcaa95558e6572
fr3qu3ncy <kilicbatuhan1205@gmail.com>
2024-06-09 16:00:17 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const execSync = require('child_process').execSync;
+
+try {
+  const actualVersion = core.getInput('actual_version');
+  const latestRelease = core.getInput('latest_release');
+
+  console.log(`Actual version: ${actualVersion}`);
+  console.log(`Latest release: ${latestRelease}`);
+
+  // Get number after last "-" in latestRelease and parse it to int
+  let oldNumber = parseInt(latestRelease.substring(latestRelease.lastIndexOf("-") + 1));
+  if (isNaN(oldNumber)) {
+    oldNumber = 0;
+  }
+
+  console.log(`Old number: ${oldNumber}`)
+
+  // Increment oldNumber
+  const newNumber = oldNumber + 1;
+
+  const newVersion = actualVersion + "-beta-" + newNumber;
+
+  console.log(output.toString());
+
+  core.setOutput("new_version", newVersion);
+
+  console.log(`Set new version to: ${newVersion}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
